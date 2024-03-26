8eced37372c97db153e34e6bfddb208456efbcb8
Черепахин Василий <vasi.che@gmail.com>
2024-03-26 17:43:57 +0500

    add test simple '4+3=7'

--- __tests__/sum.spec.js
+++ __tests__/sum.spec.js
@@ -2,3 +2,8 @@ test('2+3=5', () => {
     const sum = require('../src/sample/sum');
     expect(sum(2, 3)).toBe(5);
 })
+
+test('4+3=7', () => {
+    const sum = require('../src/sample/sum');
+    expect(sum(3, 4)).toBe(7);
+})
